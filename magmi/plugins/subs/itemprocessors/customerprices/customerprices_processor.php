5f949afc735d9db1842f127da2b08484cd7c55d7
Andreas Bilz <anti@herooutoftime.com>
2016-08-26 17:07:13 +0200

    Initial commit

---
+++ magmi/plugins/subs/itemprocessors/customerprices/customerprices_processor.php
@@ -0,0 +1,100 @@
+<?php
+
+class CustomerpricesProcessor extends Magmi_ItemProcessor {
+
+	protected $_cpcol = array();
+
+
+	public function getPluginInfo()
+	{
+		return array(
+			'name'=>'Customer Price Importer',
+			'author'=>'Andreas Bilz,herooutoftime',
+			'version'=>'0.0.1'
+		);
+	}
+
+	public function processItemAfterId(&$item, $params = null)
+	{
+		$pid = $params["product_id"];
+		$cpn = $this->tablename($this->getParam("CUSTPRI:table", "customerprices_prices"));
+		$cpcol = array_intersect(array_keys($this->_cpcol), array_keys($item));
+
+		// Do nothing if item has no customer price info or has not change
+		if (count($cpcol) == 0)
+			return true;
+
+		// Get all Mage-customers by their wawi-customer-id
+		// CAUTION: Multiple Mage-customers possible
+		$customers = $this->getCustomers();
+
+		// Remove previously created entries
+		$sql = "DELETE FROM {$cpn} WHERE product_id = {$pid}";
+		$this->delete($sql);
+
+		// Prepare basic SQL-query
+		$sql = "INSERT INTO $cpn
+			(customer_id, product_id, store_id, qty, price, special_price, customer_email, created_at, updated_at) VALUES (:customer_id, :product_id, :store_id, :qty, :price, :special_price, :customer_email, :created_at, :updated_at)";
+
+		foreach ($cpcol as $k) {
+			// get customer price column info
+			$cpinf = $this->_cpcol[$k];
+			$_wawi_knr = $cpinf['name'];
+			foreach ($customers[$_wawi_knr] as $customer) {
+				$price = str_replace(",", ".", $item[$k]);
+				$special_price = str_replace(",", ".", $item[$k]);
+				$data = array(
+					'customer_id' => $customer['id'],
+					'product_id' => $pid,
+					'store_id' => '0',
+					'qty' => 1,
+					'price' => $price,
+					'special_price' => $special_price,
+					'customer_email' => $customer['email'],
+					'created_at' => date('Y-m-d H:i:s'),
+					'updated_at' => date('Y-m-d H:i:s'),
+				);
+				// Execute SQL-query
+				$this->insert($sql, $data);
+			}
+		}
+		return true;
+	}
+
+	public function getCustomers()
+	{
+		$wawi_customers = array_column($this->_cpcol, 'name');
+		$customerColl = Mage::getModel("customer/customer")->getCollection();
+		$customerColl
+			->addAttributeToSelect('wawi_customer_id')
+			->addAttributeToFilter('wawi_customer_id', array('in' => $wawi_customers))
+			->getSelect()
+			->reset(Zend_Db_Select::COLUMNS)
+			->columns('entity_id AS id')
+			->columns('at_wawi_customer_id.value AS wawi_customer_id')
+			->columns('email');
+		$customers = $customerColl->getData();
+		foreach ($customers as $index => $customer) {
+			$customers[$customer['wawi_customer_id']][] = $customer;
+			unset($customers[$index]);
+		}
+		return $customers;
+	}
+
+	public function processColumnList(&$cols, $params = null)
+	{
+		$pattern = $this->getParam("CUSTPRI:column_name", "customer_price");
+		foreach ($cols as $col) {
+			if (preg_match("|{$pattern}:(.*)|", $col, $matches)) {
+				$tpinf = array("name" => $matches[1], "id" => null);
+				$this->_cpcol[$col] = $tpinf;
+			}
+		}
+		return true;
+	}
+
+	public function initialize($params)
+	{
+
+	}
+}
---
+++ magmi/plugins/subs/itemprocessors/customerprices/options_panel.php
@@ -0,0 +1,10 @@
+<ul class="formline">
+	<li class="label">Customer Prices Table</li>
+	<li class="value"><input type="text" name="CUSTPRI:table"
+	                         value="<?php echo $this->getParam("CUSTPRI:table", "customerprices_prices")?>"></input></li>
+</ul>
+<ul class="formline">
+	<li class="label">Customer Prices Column Value (DB or CSV)</li>
+	<li class="value"><input type="text" name="CUSTPRI:column_name"
+	                         value="<?php echo $this->getParam("CUSTPRI:column_name", "customer_price")?>"></input></li>
+</ul>
